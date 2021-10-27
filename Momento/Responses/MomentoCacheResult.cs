a865cc279e6a0903f7d4d0f004758751335a9179
Matt Straathof <matt.straat@gmail.com>
2021-10-27 16:08:43 -0700

    fix: use enum instead of static class members

--- Momento/Responses/MomentoCacheResult.cs
+++ Momento/Responses/MomentoCacheResult.cs
@@ -2,34 +2,11 @@
 
 namespace MomentoSdk.Responses
 {
-    public class MomentoCacheResult
+    public enum MomentoCacheResult
     {
-
-        public string Value { get; private set; }
-        private MomentoCacheResult(string value) { Value = value; }
-
-
-        public static MomentoCacheResult Ok { get { return new MomentoCacheResult("OK"); } }
-        public static MomentoCacheResult Hit { get { return new MomentoCacheResult("HIT"); } }
-        public static MomentoCacheResult Miss { get { return new MomentoCacheResult("MISS"); } }
-        public static MomentoCacheResult Unknown { get { return new MomentoCacheResult("UNKNOWN"); } }
-
-        public override bool Equals(object obj)
-        {
-            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
-            {
-                return false;
-            }
-            else
-            {
-                MomentoCacheResult r = (MomentoCacheResult)obj;
-                return (Value == r.Value);
-            }
-        }
-
-        public override int GetHashCode()
-        {
-            return Value.GetHashCode();
-        }
+        Ok,
+        Hit,
+        Miss,
+        Unknown
     }
 }
