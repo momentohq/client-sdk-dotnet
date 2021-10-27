366cccd3f52b64938eda84d6010a67f645db4eda
Matt Straathof <matt.straat@gmail.com>
2021-10-27 15:38:25 -0700

    fix: make cache service exception public

--- Momento/Exceptions/CacheServiceException.cs
+++ Momento/Exceptions/CacheServiceException.cs
@@ -5,7 +5,7 @@ namespace MomentoSdk.Exceptions
     /// This is the base class for all exceptions thrown by the Cache Service. This exception
     /// will be thrown if there is an unknown exception thrown by the Cache Service.
     /// </summary>
-    abstract class CacheServiceException : MomentoServiceException
+    public abstract class CacheServiceException : MomentoServiceException
     {
         public CacheServiceException(String message) : base(message)
         {
