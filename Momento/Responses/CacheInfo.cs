0cb6085fe4067bc0c957411d9db1de751a47ed6f
Michael Landis <michael@momentohq.com>
2022-05-19 18:18:07 -0700

    Improve control client test coverage.

--- Momento/Responses/CacheInfo.cs
+++ Momento/Responses/CacheInfo.cs
@@ -1,6 +1,8 @@
-﻿namespace MomentoSdk.Responses
+﻿using System;
+
+namespace MomentoSdk.Responses
 {
-    public class CacheInfo
+    public class CacheInfo : IEquatable<CacheInfo>
     {
         private readonly string name;
         public CacheInfo(string cachename)
@@ -11,6 +13,30 @@
         public string Name()
         {
             return name;
+        }
+
+        // override object.Equals
+        public bool Equals(CacheInfo other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            return this.name == other.name;
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if (obj == null)
+                return false;
+
+            return Equals(obj as CacheInfo);
+        }
+
+        public override int GetHashCode()
+        {
+            return name.GetHashCode();
         }
     }
 }
--- MomentoIntegrationTest/SimpleCacheControlPlaneTests.cs
+++ MomentoIntegrationTest/SimpleCacheControlPlaneTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Xunit;
 using MomentoSdk.Exceptions;
 using MomentoSdk.Responses;
@@ -11,11 +12,11 @@ namespace MomentoIntegrationTest
         private string authKey = Environment.GetEnvironmentVariable("TEST_AUTH_TOKEN");
 
         [Fact]
-        public void DeleteCache_CacheDoesntExist_NotFoundException()
+        public void SimpleCacheClientConstructor_BadRequestTimeout_ThrowsException()
         {
             uint defaultTtlSeconds = 10;
-            SimpleCacheClient client = new SimpleCacheClient(authKey, defaultTtlSeconds);
-            Assert.Throws<NotFoundException>(() => client.DeleteCache("non existant cache"));
+            uint requestTimeoutMilliseconds = 0;
+            Assert.Throws<InvalidArgumentException>(() => new SimpleCacheClient(authKey, defaultTtlSeconds, requestTimeoutMilliseconds));
         }
 
         [Fact]
@@ -25,26 +26,74 @@ namespace MomentoIntegrationTest
             Assert.Throws<InvalidArgumentException>(() => new SimpleCacheClient("eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJpbnRlZ3JhdGlvbiJ9.ZOgkTs", defaultTtlSeconds));
         }
 
-
         [Fact]
-        public void ListCaches_OneCache_HappyPath()
+        public void DeleteCache_CacheDoesntExist_NotFoundException()
         {
             uint defaultTtlSeconds = 10;
             SimpleCacheClient client = new SimpleCacheClient(authKey, defaultTtlSeconds);
-            string cacheName = Guid.NewGuid().ToString();
-            client.CreateCache(cacheName);
-            ListCachesResponse result = client.ListCaches();
-            List<CacheInfo> caches = result.Caches();
-            Assert.True(caches.Exists(item => item.Name().Equals(cacheName)));
-            client.DeleteCache(cacheName);
+            Assert.Throws<NotFoundException>(() => client.DeleteCache("non existant cache"));
         }
 
+        // Tests: creating a cache, listing a cache, and deleting a cache.
         [Fact]
-        public void SimpleCacheClientConstructor_BadRequestTimeout_ThrowsException()
+        public void ListCaches_OneCache_HappyPath()
         {
-            uint defaultTtlSeconds = 10;
-            uint requestTimeoutMilliseconds = 0;
-            Assert.Throws<InvalidArgumentException>(() => new SimpleCacheClient(authKey, defaultTtlSeconds, requestTimeoutMilliseconds));
+            // Create cache
+            SimpleCacheClient client = new SimpleCacheClient(authKey, defaultTtlSeconds: 10);
+            string cacheName = Guid.NewGuid().ToString();
+            client.CreateCache(cacheName);
+
+            // Test cache exists
+            ListCachesResponse result = client.ListCaches();
+            List<CacheInfo> caches = result.Caches();
+            Assert.True(caches.Contains(new CacheInfo(cacheName)));
+
+            // Test deleting cache
+            client.DeleteCache(cacheName);
+            result = client.ListCaches();
+            caches = result.Caches();
+            Assert.False(caches.Contains(new CacheInfo(cacheName)));
+        }
+
+        /* TODO: ensure local client supports many caches
+        [Fact]
+        public void ListCaches_Iteration_HappyPath()
+        {
+            // Create caches
+            SimpleCacheClient client = new SimpleCacheClient(authKey, defaultTtlSeconds: 10);
+            List<String> cacheNames = new List<String>();
+            foreach (int val in Enumerable.Range(1, 20))
+            {
+                String cacheName = Guid.NewGuid().ToString();
+                cacheNames.Add(cacheName);
+                client.CreateCache(cacheName);
+            }
+
+            // List caches
+            HashSet<String> retrievedCaches = new HashSet<string>();
+            ListCachesResponse result = client.ListCaches();
+            do
+            {
+                foreach (CacheInfo cache in result.Caches())
+                {
+                    retrievedCaches.Add(cache.Name());
+                }
+            } while (result.NextPageToken != null);
+
+            // Cleanup
+            foreach (String cacheName in cacheNames)
+            {
+                client.DeleteCache(cacheName);
+            }
+        }
+        */
+
+        [Fact]
+        public void ListCaches_BadNextToken_NoException()
+        {
+            // A bad next token does not throw an exception
+            SimpleCacheClient client = new SimpleCacheClient(authKey, defaultTtlSeconds: 10);
+            client.ListCaches(nextPageToken: "hello world");
         }
     }
 }
