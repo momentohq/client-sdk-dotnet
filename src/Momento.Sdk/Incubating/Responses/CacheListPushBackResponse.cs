64ba8a466d2b457c841a010b0a037288721d692e
Michael Landis <michael@momentohq.com>
2022-09-13 10:04:57 -0700

    Document responses

--- src/Momento.Sdk/Incubating/Responses/CacheListPushBackResponse.cs
+++ src/Momento.Sdk/Incubating/Responses/CacheListPushBackResponse.cs
@@ -2,8 +2,14 @@ using Momento.Protos.CacheClient;
 
 namespace Momento.Sdk.Incubating.Responses;
 
+/// <summary>
+/// The result of a <c>ListPushBack</c> command
+/// </summary>
 public class CacheListPushBackResponse
 {
+    /// <summary>
+    /// The length of the list post-push (and post-truncate, if that applies)
+    /// </summary>
     public int ListLength { get; private set; }
 
     public CacheListPushBackResponse(_ListPushBackResponse response)
--- src/Momento.Sdk/Incubating/Responses/CacheListPushFrontResponse.cs
+++ src/Momento.Sdk/Incubating/Responses/CacheListPushFrontResponse.cs
@@ -2,8 +2,14 @@ using Momento.Protos.CacheClient;
 
 namespace Momento.Sdk.Incubating.Responses;
 
+/// <summary>
+/// The result of a <c>ListPushFront</c> command
+/// </summary>
 public class CacheListPushFrontResponse
 {
+    /// <summary>
+    /// The length of the list post-push (and post-truncate, if that applies)
+    /// </summary>
     public int ListLength { get; private set; }
 
     public CacheListPushFrontResponse(_ListPushFrontResponse response)
