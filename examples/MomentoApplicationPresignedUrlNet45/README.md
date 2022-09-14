9794827da03f0aa64ed0ee971e090c6ccdc14e4a
Chris Price <cprice404@users.noreply.github.com>
2022-09-13 17:55:05 -0700

    feat: copy dotnet examples from examples repo (#148)
    * feat: copy dotnet examples from examples repo
    
    We are in the process of moving the examples out of the old
    examples repo and into the individual SDK repos.  This commit
    copies the dotnet examples over; no changes.
    
    Also copies over Pete's dependabot/examples CI work.

---
+++ examples/MomentoApplicationPresignedUrlNet45/README.md
@@ -0,0 +1,15 @@
+## MomentoSdkDotnet45
+
+SDK is built for target framework .NET Framework 4.5 or above. Only MomentoSigner is supported.
+
+## To add to your project
+
+In Visual Studio project, open the NuGet Package Manager. 
+
+Add package source https://momento.jfrog.io/artifactory/api/nuget/nuget-public.
+
+Install the package MomentoSdkDotnet45.
+
+## Example Code for Presigned URL Creation
+
+[Program.cs](Program.cs)
---
+++ examples/MomentoFSharpApplication/Program.fs
@@ -0,0 +1,34 @@
+﻿open Momento.Sdk
+open Momento.Sdk.Exceptions
+open System
+
+
+let MOMENTO_AUTH_TOKEN = Environment.GetEnvironmentVariable("MOMENTO_AUTH_TOKEN")
+let CACHE_NAME = "cache"
+let DEFAULT_TTL_SECONDS = 60u
+
+let exerciseCache() = (
+    printfn "Howdy"
+    using(new SimpleCacheClient(MOMENTO_AUTH_TOKEN, DEFAULT_TTL_SECONDS)) (fun client ->
+        try
+            client.CreateCache(CACHE_NAME)
+        with
+            | :? AlreadyExistsException -> printfn $"Cache with name {CACHE_NAME} already exists\n"; null
+        |> ignore
+        
+
+        printfn("Listing caches:")
+        let resp = client.ListCaches()
+        for cacheInfo in resp.Caches do
+            printfn $"{cacheInfo.Name}"
+        
+
+        null
+    )
+
+)
+
+[<EntryPoint>]
+let main(argv :string[]) =
+    exerciseCache()
+    0
