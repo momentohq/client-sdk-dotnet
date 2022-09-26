a458abf2a0cd0c0f031487ce014ddd29353e57ef
pgautier404 <pgautier404@users.noreply.github.com>
2022-09-26 17:01:15 -0500

    fix: fix transport details nullability in constructors (#189)
    * fix: remove CacheGetStatus enum
    
    * fix: fix transport details nullability in constructors
    
    Co-authored-by: Pete Gautier <pete@momentohq.com>

--- src/Momento.Sdk/Exceptions/TimeoutException.cs
+++ src/Momento.Sdk/Exceptions/TimeoutException.cs
@@ -7,7 +7,7 @@ using System;
 /// </summary>
 public class TimeoutException : SdkException
 {
-    public TimeoutException(string message, MomentoErrorTransportDetails? transportDetails=null, Exception? e=null) : base(MomentoErrorCode.TIMEOUT_ERROR, message, transportDetails, e)
+    public TimeoutException(string message, MomentoErrorTransportDetails transportDetails, Exception? e=null) : base(MomentoErrorCode.TIMEOUT_ERROR, message, transportDetails, e)
     {
         this.MessageWrapper = "The client's configured timeout was exceeded; you may need to use a Configuration with more lenient timeouts";
     }
