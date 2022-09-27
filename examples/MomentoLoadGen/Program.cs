c9dafa8faba36ce3021aa6089cb392b2efab4374
Michael Landis <michael@momentohq.com>
2022-09-27 12:06:27 -0700

    fix: update example code (#199)
    Update MomentoApplication and MomentoLoadGen to latest version

--- examples/MomentoLoadGen/Program.cs
+++ examples/MomentoLoadGen/Program.cs
@@ -104,7 +104,7 @@ namespace MomentoLoadGen
 
             try
             {
-                momento.CreateCache(CACHE_NAME);
+                await momento.CreateCacheAsync(CACHE_NAME);
             }
             catch (AlreadyExistsException)
             {
