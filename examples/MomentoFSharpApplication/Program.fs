9dc9b7c07bfbc13056ee8875a0fb5ecc58f3c9bc
Chris Price <chris@momentohq.com>
2022-09-27 12:09:08 -0700

    chore: some final tweaks for README prior to bug bash
    * bumps stability to 'beta'
    * Adds a simpler usage example for embedding into the README
    * Updates f# example to latest SDK

--- examples/MomentoFSharpApplication/Program.fs
+++ examples/MomentoFSharpApplication/Program.fs
@@ -13,15 +13,10 @@ let DEFAULT_TTL_SECONDS = 60u
 let exerciseCache() = (
     printfn "Howdy"
     using(new SimpleCacheClient(Configurations.Laptop.Latest, MOMENTO_AUTH_TOKEN, DEFAULT_TTL_SECONDS)) (fun client ->
-        try
-            client.CreateCache(CACHE_NAME)
-        with
-            | :? AlreadyExistsException -> printfn $"Cache with name {CACHE_NAME} already exists\n"; null
-        |> ignore
+        let createCacheResult = client.CreateCacheAsync(CACHE_NAME) |> Async.AwaitTask
         
-
-        printfn("Listing caches:")
-        let resp = client.ListCaches()
+        printfn("Listing caches:")        
+        let resp = client.ListCachesAsync(MOMENTO_AUTH_TOKEN) |> Async.AwaitTask |> Async.RunSynchronously
         let _ = match resp with
                 | :? ListCachesResponse.Success as successResponse ->
                     for cacheInfo in successResponse.Caches do
