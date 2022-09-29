96f5751fd0505b15a3cf59fd3dd20bd07c61031c
pgautier404 <pgautier404@users.noreply.github.com>
2022-09-29 13:43:51 -0700

    fix: update sdk examples (#246)
    * feat: change cache already exists error from exception to response type
    
    * fix: example code improvements
    
    * chore: update sdk version to 0.32.0
    
    * standardize variable names
    
    * more variable name standardization
    
    * PR feedback tweaks
    
    * use safer example cache name
    
    Co-authored-by: Pete Gautier <pete@momentohq.com>

--- examples/MomentoApplication/Program.cs
+++ examples/MomentoApplication/Program.cs
@@ -6,43 +6,93 @@ using Momento.Sdk.Config;
 using Momento.Sdk.Exceptions;
 using Momento.Sdk.Responses;
 
-namespace MomentoApplication
+string MOMENTO_AUTH_TOKEN = Environment.GetEnvironmentVariable("MOMENTO_AUTH_TOKEN");
+// Use a GUID for the cache name so it's unlikely to exist already.
+const string CACHE_NAME = "momento-example";
+const string KEY = "MyKey";
+const string VALUE = "MyData";
+const uint DEFAULT_TTL_SECONDS = 60;
+
+using (SimpleCacheClient client = new SimpleCacheClient(Configurations.Laptop.Latest, MOMENTO_AUTH_TOKEN, DEFAULT_TTL_SECONDS))
 {
-    class Program
+    Console.WriteLine($"Creating cache {CACHE_NAME}");
+    var createCacheResponse = await client.CreateCacheAsync(CACHE_NAME);
+    // Check the create response for an error and handle as appropriate.
+    if (createCacheResponse is CreateCacheResponse.Error createCacheError)
     {
-        static readonly String MOMENTO_AUTH_TOKEN = Environment.GetEnvironmentVariable("MOMENTO_AUTH_TOKEN");
-        static readonly String CACHE_NAME = "cache";
-        static readonly String KEY = "MyKey";
-        static readonly String VALUE = "MyData";
-        static readonly uint DEFAULT_TTL_SECONDS = 60;
-
-        async static Task Main(string[] args)
+        if (createCacheError.ErrorCode == MomentoErrorCode.LIMIT_EXCEEDED_ERROR)
         {
-            using SimpleCacheClient client = new SimpleCacheClient(Configurations.Laptop.Latest, MOMENTO_AUTH_TOKEN, DEFAULT_TTL_SECONDS);
-            var createCacheResult = await client.CreateCacheAsync(CACHE_NAME);
-
-            Console.WriteLine("Listing caches:");
-            String token = null;
-            do
-            {
-                ListCachesResponse resp = await client.ListCachesAsync(token);
-                if (resp is ListCachesResponse.Success successResult)
-                {
-                    foreach (CacheInfo cacheInfo in successResult.Caches)
-                    {
-                        Console.WriteLine(cacheInfo.Name);
-                    }
-                    token = successResult.NextPageToken;
-                }
-            } while (!String.IsNullOrEmpty(token));
-            Console.WriteLine($"\nSetting key: {KEY} with value: {VALUE}");
-            await client.SetAsync(CACHE_NAME, KEY, VALUE);
-            Console.WriteLine($"\nGet value for  key: {KEY}");
-            CacheGetResponse getResponse = await client.GetAsync(CACHE_NAME, KEY);
-            if (getResponse is CacheGetResponse.Hit hitResponse)
-            {
-                Console.WriteLine($"\nLookedup value: {hitResponse.String()}, Stored value: {VALUE}");
-            }
+            Console.WriteLine("Error: cache limit exceeded. We need to talk to support@moentohq.com! Exiting.");
+        } else
+        {
+            Console.WriteLine($"Error creating cache: {createCacheError.Message}. Exiting.");
         }
+        // Any error is considered fatal.
+        Environment.Exit(1);
     }
+    // If there's already a cache by this name, alert the user.
+    if (createCacheResponse is CreateCacheResponse.CacheAlreadyExists)
+    {
+        Console.WriteLine($"A cache with the name {CACHE_NAME} already exists");
+    }
+
+    Console.WriteLine("\nListing caches:");
+    string token = null;
+    do
+    {
+        ListCachesResponse listCachesResponse = await client.ListCachesAsync(token);
+        if (listCachesResponse is ListCachesResponse.Success listCachesSuccess)
+        {
+            foreach (CacheInfo cacheInfo in listCachesSuccess.Caches)
+            {
+                Console.WriteLine($"- {cacheInfo.Name}");
+            }
+            token = listCachesSuccess.NextPageToken;
+        } else if (listCachesResponse is ListCachesResponse.Error listCachesError)
+        {
+            // We do not consider this a fatal error, so we just report it.
+            Console.WriteLine($"Error listing caches: {listCachesError.Message}");
+            break;
+        }
+    } while (!String.IsNullOrEmpty(token));
+
+    Console.WriteLine($"\nSetting key: {KEY} with value: {VALUE}");
+    var setResponse = await client.SetAsync(CACHE_NAME, KEY, VALUE);
+    if (setResponse is CacheSetResponse.Error setError)
+    {
+        // Warn the user of the error and exit.
+        Console.WriteLine($"Error setting value: {setError.Message}. Exiting.");
+        Environment.Exit(1);
+    }
+
+    Console.WriteLine($"\nGetting value for key: {KEY}");
+    CacheGetResponse getResponse = await client.GetAsync(CACHE_NAME, KEY);
+    if (getResponse is CacheGetResponse.Hit getHit)
+    {
+        Console.WriteLine($"Looked up value: {getHit.String()}, Stored value: {VALUE}");
+    } else if (getResponse is CacheGetResponse.Miss)
+    {
+        // This shouldn't be fatal but should be reported.
+        Console.WriteLine($"Error: got a cache miss for {KEY}!");
+    } else if (getResponse is CacheGetResponse.Error getError) {
+        // Also not considered fatal.
+        Console.WriteLine($"Error getting value: {getError.Message}!");
+    }
+
+    Console.WriteLine($"\nDeleting key {KEY}");
+    var deleteKeyResponse = await client.DeleteAsync(CACHE_NAME, KEY);
+    if (deleteKeyResponse is CacheDeleteResponse.Error deleteKeyError) {
+        // Also not considred fatal.
+        Console.WriteLine($"Error deleting key: {deleteKeyError.Message}!");
+    }
+
+    Console.WriteLine($"\nDeleting cache {CACHE_NAME}");
+    var deleteCacheResponse = await client.DeleteCacheAsync(CACHE_NAME);
+    if (deleteCacheResponse is DeleteCacheResponse.Error deleteCacheError)
+    {
+        // Report fatal error and exit
+        Console.WriteLine("Error deleting cache: {deleteCacheError.Message}. Exiting.");
+        Environment.Exit(1);
+    }
+    Console.WriteLine("\nProgram has completed successfully.");
 }
--- examples/MomentoUsage/Program.cs
+++ examples/MomentoUsage/Program.cs
@@ -2,22 +2,33 @@
 using Momento.Sdk.Config;
 using Momento.Sdk.Responses;
 
-String MOMENTO_AUTH_TOKEN = Environment.GetEnvironmentVariable("MOMENTO_AUTH_TOKEN");
-String CACHE_NAME = "cache";
-String KEY = "MyKey";
-String VALUE = "MyData";
-uint DEFAULT_TTL_SECONDS = 60;
+string MOMENTO_AUTH_TOKEN = Environment.GetEnvironmentVariable("MOMENTO_AUTH_TOKEN") ?? "";
+const string CACHE_NAME = "cache";
+const string KEY = "MyKey";
+const string VALUE = "MyData";
+const uint DEFAULT_TTL_SECONDS = 60;
 
 using (SimpleCacheClient client = new SimpleCacheClient(Configurations.Laptop.Latest, MOMENTO_AUTH_TOKEN, DEFAULT_TTL_SECONDS))
 {
-    var createCacheResult = await client.CreateCacheAsync(CACHE_NAME);
+    var createCacheResponse = await client.CreateCacheAsync(CACHE_NAME);
+    if (createCacheResponse is CreateCacheResponse.Error createError) {
+        Console.WriteLine($"Error creating cache: {createError.Message}. Exiting.");
+        Environment.Exit(1);
+    }
 
-    Console.WriteLine($"\nSetting key: {KEY} with value: {VALUE}");
-    await client.SetAsync(CACHE_NAME, KEY, VALUE);
-    Console.WriteLine($"\nGet value for  key: {KEY}");
+    Console.WriteLine($"Setting key: {KEY} with value: {VALUE}");
+    var setResponse = await client.SetAsync(CACHE_NAME, KEY, VALUE);
+    if (setResponse is CacheSetResponse.Error setError) {
+        Console.WriteLine($"Error setting value: {setError.Message}. Exiting.");
+        Environment.Exit(1);
+    }
+
+    Console.WriteLine($"Get value for key: {KEY}");
     CacheGetResponse getResponse = await client.GetAsync(CACHE_NAME, KEY);
     if (getResponse is CacheGetResponse.Hit hitResponse)
     {
-        Console.WriteLine($"\nLookedup value: {hitResponse.String()}, Stored value: {VALUE}");
+        Console.WriteLine($"Looked up value: {hitResponse.String()}, Stored value: {VALUE}");
+    } else if (getResponse is CacheGetResponse.Error getError) {
+        Console.WriteLine($"Error getting value: {getError.Message}");
     }
 }
