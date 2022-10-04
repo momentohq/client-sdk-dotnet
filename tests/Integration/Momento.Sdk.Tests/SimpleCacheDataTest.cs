871d5e72c058376f2b942b60a1b2957d3970384e
Pete Gautier <pete@momentohq.com>
2022-10-04 15:23:55 -0700

    remove unneeded usings

--- tests/Integration/Momento.Sdk.Tests/SimpleCacheDataTest.cs
+++ tests/Integration/Momento.Sdk.Tests/SimpleCacheDataTest.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Momento.Sdk.Auth;
-using Momento.Sdk.Config;
 
 namespace Momento.Sdk.Tests;
 
