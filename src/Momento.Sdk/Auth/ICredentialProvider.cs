a7a6ddb655d50901ba8a148d0af9985e28a599e3
erikatharp <eem@momentohq.com>
2022-10-06 16:42:58 -0700

    fix: re-enable warning and address 20 warnings

--- src/Momento.Sdk/Auth/ICredentialProvider.cs
+++ src/Momento.Sdk/Auth/ICredentialProvider.cs
@@ -2,9 +2,21 @@ namespace Momento.Sdk.Auth;
 
 using System.Collections.Generic;
 
+/// <summary>
+/// Interface for credentials
+/// </summary>
 public interface ICredentialProvider
 {
+    /// <summary>
+    /// JWT provided by user
+    /// </summary>
     string AuthToken { get; }
-    string ControlEndpoint {get; }
+    /// <summary>
+    /// Control endpoint extracted from JWT
+    /// </summary>
+    string ControlEndpoint { get; }
+    /// <summary>
+    /// Cache endpoint extracted from JWT
+    /// </summary>
     string CacheEndpoint { get; }
 }
--- src/Momento.Sdk/Config/Configuration.cs
+++ src/Momento.Sdk/Config/Configuration.cs
@@ -6,18 +6,33 @@ using Momento.Sdk.Config.Transport;
 
 namespace Momento.Sdk.Config;
 
+/// <inheritdoc cref="Momento.Sdk.Config.IConfiguration" />
 public class Configuration : IConfiguration
 {
+    /// <inheritdoc cref="Momento.Sdk.Config.Retry.IRetryStrategy" />
     public IRetryStrategy RetryStrategy { get; }
+    /// <inheritdoc cref="Momento.Sdk.Config.Middleware.IMiddleware" />
     public IList<IMiddleware> Middlewares { get; }
+    /// <inheritdoc cref="Momento.Sdk.Config.Transport.ITransportStrategy" />
     public ITransportStrategy TransportStrategy { get; }
 
+    /// <summary>
+    /// <inheritdoc cref="Momento.Sdk.Config.IConfiguration" />
+    /// </summary>
+    /// <param name="retryStrategy">Defines a contract for how and when to retry a request</param>
+    /// <param name="transportStrategy">This is responsible for configuring network tunables.</param>
     public Configuration(IRetryStrategy retryStrategy, ITransportStrategy transportStrategy)
         : this(retryStrategy, new List<IMiddleware>(), transportStrategy)
     {
 
     }
 
+    /// <summary>
+    /// <inheritdoc cref="Momento.Sdk.Config.IConfiguration" />
+    /// </summary>
+    /// <param name="retryStrategy">Defines a contract for how and when to retry a request</param>
+    /// <param name="middlewares">The Middleware interface allows the Configuration to provide a higher-order function that wraps all requests.</param>
+    /// <param name="transportStrategy">This is responsible for configuring network tunables.</param>
     public Configuration(IRetryStrategy retryStrategy, IList<IMiddleware> middlewares, ITransportStrategy transportStrategy)
     {
         this.RetryStrategy = retryStrategy;
@@ -25,21 +40,41 @@ public class Configuration : IConfiguration
         this.TransportStrategy = transportStrategy;
     }
 
+    /// <summary>
+    ///  Configures retry strategy
+    /// </summary>
+    /// <param name="retryStrategy">Defines a contract for how and when to retry a request</param>
+    /// <returns>Configuration object with custom retry strategy provided</returns>
     public IConfiguration WithRetryStrategy(IRetryStrategy retryStrategy)
     {
         return new Configuration(retryStrategy, Middlewares, TransportStrategy);
     }
 
+    /// <summary>
+    ///  Configures middlewares
+    /// </summary>
+    /// <param name="middlewares">The Middleware interface allows the Configuration to provide a higher-order function that wraps all requests.</param>
+    /// <returns>Configuration object with custom middlewares provided</returns>
     public IConfiguration WithMiddlewares(IList<IMiddleware> middlewares)
     {
         return new Configuration(RetryStrategy, middlewares, TransportStrategy);
     }
 
+    /// <summary>
+    ///  Configures transport trategy
+    /// </summary>
+    /// <param name="transportStrategy">This is responsible for configuring network tunables.</param>
+    /// <returns>Configuration object with custom transport strategy provided</returns>
     public IConfiguration WithTransportStrategy(ITransportStrategy transportStrategy)
     {
         return new Configuration(RetryStrategy, Middlewares, transportStrategy);
     }
 
+    /// <summary>
+    ///  Configures middlewares
+    /// </summary>
+    /// <param name="additionalMiddlewares">The Middleware interface allows the Configuration to provide a higher-order function that wraps all requests.</param>
+    /// <returns>Configuration object with custom middlewares provided</returns>
     public Configuration WithAdditionalMiddlewares(IList<IMiddleware> additionalMiddlewares)
     {
         return new(
@@ -49,6 +84,11 @@ public class Configuration : IConfiguration
         );
     }
 
+    /// <summary>
+    ///  Configures client timeout for transport strategy
+    /// </summary>
+    /// <param name="clientTimeoutMillis">Client timeout in milliseconds.</param>
+    /// <returns>Configuration object with client timeout provided</returns>
     public Configuration WithClientTimeoutMillis(uint clientTimeoutMillis)
     {
         return new(
