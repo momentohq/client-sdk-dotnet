e9a6d88018c68ef410b0abfb45a417da538b1bd1
Michael Landis <michael@momentohq.com>
2022-11-14 11:04:37 -0800

    feat: add truncate string extension method
    Adds a string extension method to truncate. Allows a configurable
    total length and fill value.

---
+++ src/Momento.Sdk/Internal/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Text;
+
+namespace Momento.Sdk.Internal.ExtensionMethods;
+
+/// <summary>
+/// Defines extension methods to support pretty printing.
+/// </summary>
+public static class StringExtensions
+{
+    /// <summary>
+    /// Truncates a string to a max length, eliding with a fill value.
+    /// </summary>
+    /// <param name="str">The string to truncate.</param>
+    /// <param name="totalLength">Maximum allowed input length.</param>
+    /// <param name="fillValue">The string to use to elide the input when it exceeds the max length.</param>
+    /// <returns>The original string if not too long, else the truncated string.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static string Truncate(this string str, int totalLength = 20, string fillValue = "...")
+    {
+        if (str.Length <= totalLength)
+        {
+            return str;
+        }
+
+        if (fillValue.Length >= totalLength)
+        {
+            throw new ArgumentException($"fillValue was larger than totalLength: {fillValue} vs {totalLength}");
+        }
+
+        var firstHalf = totalLength / 2 - fillValue.Length / 2;
+        if (totalLength % 2 == 0)
+        {
+            firstHalf -= (fillValue.Length % 2);
+        }
+
+        var sb = new StringBuilder();
+        sb.Append(str.Substring(0, firstHalf));
+        sb.Append(fillValue);
+        sb.Append(str.Substring(str.Length - (totalLength - firstHalf - fillValue.Length)));
+        return sb.ToString();
+    }
+}
+
+
