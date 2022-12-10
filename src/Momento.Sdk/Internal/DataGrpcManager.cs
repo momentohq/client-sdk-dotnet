cc0b70aab8968e3396584bc10ce16736b8a4b3f9
Michael Landis <michael@momentohq.com>
2022-12-09 18:40:04 -0800

    chore: remove unused list erase method
    Removing list erase pending customer demand.

--- src/Momento.Sdk/Internal/DataGrpcManager.cs
+++ src/Momento.Sdk/Internal/DataGrpcManager.cs
@@ -35,7 +35,6 @@ public interface IDataClient
     public Task<_ListFetchResponse> ListFetchAsync(_ListFetchRequest request, CallOptions callOptions);
     public Task<_ListRemoveResponse> ListRemoveAsync(_ListRemoveRequest request, CallOptions callOptions);
     public Task<_ListLengthResponse> ListLengthAsync(_ListLengthRequest request, CallOptions callOptions);
-    public Task<_ListEraseResponse> ListEraseAsync(_ListEraseRequest request, CallOptions callOptions);
     public Task<_ListConcatenateFrontResponse> ListConcatenateFrontAsync(_ListConcatenateFrontRequest request, CallOptions callOptions);
     public Task<_ListConcatenateBackResponse> ListConcatenateBackAsync(_ListConcatenateBackRequest request, CallOptions callOptions);
 }
@@ -165,12 +164,6 @@ public class DataClientWithMiddleware : IDataClient
     public async Task<_ListLengthResponse> ListLengthAsync(_ListLengthRequest request, CallOptions callOptions)
     {
         var wrapped = await _middlewares.WrapRequest(request, callOptions, (r, o) => _generatedClient.ListLengthAsync(r, o));
-        return await wrapped.ResponseAsync;
-    }
-
-    public async Task<_ListEraseResponse> ListEraseAsync(_ListEraseRequest request, CallOptions callOptions)
-    {
-        var wrapped = await _middlewares.WrapRequest(request, callOptions, (r, o) => _generatedClient.ListEraseAsync(r, o));
         return await wrapped.ResponseAsync;
     }
 
