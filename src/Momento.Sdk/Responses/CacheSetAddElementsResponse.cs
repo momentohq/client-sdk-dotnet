0bf65d45450c7bd3087f25e94d4ed3f94e23eeff
erikatharp <eem@momentohq.com>
2023-01-10 11:27:21 -0800

    chore: add set response classes

---
+++ src/Momento.Sdk/Responses/CacheSetAddElementsResponse.cs
@@ -0,0 +1,40 @@
+﻿using Momento.Sdk.Exceptions;
+
+namespace Momento.Sdk.Responses;
+
+public abstract class CacheSetAddElementsResponse
+{
+    public class Success : CacheSetAddElementsResponse
+    {
+    }
+    public class Error : CacheSetAddElementsResponse
+    {
+        private readonly SdkException _error;
+        public Error(SdkException error)
+        {
+            _error = error;
+        }
+
+        public SdkException Exception
+        {
+            get => _error;
+        }
+
+        public MomentoErrorCode ErrorCode
+        {
+            get => _error.ErrorCode;
+        }
+
+        public string Message
+        {
+            get => $"{_error.MessageWrapper}: {_error.Message}";
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return $"{base.ToString()}: {Message}";
+        }
+    }
+
+}
