00fefcb6f7c25a3869d7544841a3438d3e868329
Chris Price <chris@momentohq.com>
2023-03-09 09:57:28 -0800

    chore: fix docstring warning on ISimpleCacheClient interface

--- src/Momento.Sdk/ISimpleCacheClient.cs
+++ src/Momento.Sdk/ISimpleCacheClient.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 namespace Momento.Sdk
 {
+    /// <summary>
+    /// Minimum viable functionality of a cache client.
+    ///
+    /// Includes control operations and data operations.
+    /// </summary>
     [Obsolete("This interface has been renamed to ICacheClient, and is now deprecated")]
     public interface ISimpleCacheClient : ICacheClient
 	{
