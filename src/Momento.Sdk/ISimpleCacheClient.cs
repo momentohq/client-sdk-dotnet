81a636ed38e9320c39f5dd5375c2a2c1b7b1d0c1
Chris Price <chris@momentohq.com>
2023-03-09 09:41:00 -0800

    fix: Restore legacy ISimpleCacheClient interface

---
+++ src/Momento.Sdk/ISimpleCacheClient.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Momento.Sdk
+{
+    [Obsolete("This interface has been renamed to ICacheClient, and is now deprecated")]
+    public interface ISimpleCacheClient : ICacheClient
+	{
+	}
+}
+
--- src/Momento.Sdk/SimpleCacheClient.cs
+++ src/Momento.Sdk/SimpleCacheClient.cs
@@ -11,7 +11,7 @@ namespace Momento.Sdk
     /// See <see href="https://github.com/momentohq/client-sdk-dotnet/tree/main/examples">the examples directory</see> for complete workflows.
     /// </summary>
     [Obsolete("This class has been renamed to CacheClient, and is now deprecated")]
-    public class SimpleCacheClient : CacheClient
+    public class SimpleCacheClient : CacheClient, ISimpleCacheClient
 	{
         /// <summary>
         /// Client to perform operations against the Momento Serverless Cache Service
