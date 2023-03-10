6beab8fbcf85510f9fdff6e23530c5bce3448515
Chris Price <chris@momentohq.com>
2023-03-10 14:02:41 -0800

    feat: re-add legacy SimpleCacheClient type for backward compat
    Doing this is a separate commit/PR to make review easier.

---
+++ src/Momento.Sdk/ISimpleCacheClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Momento.Sdk
+{
+    /// <summary>
+    /// Minimum viable functionality of a cache client.
+    ///
+    /// Includes control operations and data operations.
+    /// </summary>
+    [Obsolete("This interface has been renamed to ICacheClient, and is now deprecated")]
+    public interface ISimpleCacheClient : ICacheClient
+    {
+    }
+}
