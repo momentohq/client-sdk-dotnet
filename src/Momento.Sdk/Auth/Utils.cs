b0d3eddee1483929cfcba164444ae7a761bb38e0
Pete Gautier <pete@momentohq.com>
2023-04-13 10:44:32 -0700

    feat: add support for v1 auth tokens

--- src/Momento.Sdk/Auth/Utils.cs
+++ src/Momento.Sdk/Auth/Utils.cs
@@ -1,17 +1,68 @@
+using System;
 using Momento.Sdk.Exceptions;
 using Momento.Sdk.Internal;
+using Newtonsoft.Json;
 
 namespace Momento.Sdk.Auth;
 
+internal class TokenAndEndpoints
+{
+    public string AuthToken { get;  }
+    public string ControlEndpoint { get; }
+    public string CacheEndpoint { get; }
+
+    public TokenAndEndpoints(string authToken, string controlEndpoint, string cacheEndpoint)
+    {
+        AuthToken = authToken;
+        ControlEndpoint = controlEndpoint;
+        CacheEndpoint = cacheEndpoint;
+    }
+}
+
+internal class Base64DecodedV1Token
+{
+    public string? api_key = null;
+    public string? endpoint = null;
+}
+
 internal static class AuthUtils
 {
-    public static Claims TryDecodeAuthToken(string authToken)
+    public static bool IsBase64String(string base64)
+    {
+        Span<byte> buffer = new Span<byte>(new byte[base64.Length]);
+        return Convert.TryFromBase64String(base64, buffer, out int bytesParsed);
+    }
+
+    public static TokenAndEndpoints TryDecodeAuthToken(string authToken)
     {
         try
         {
-            return JwtUtils.DecodeJwt(authToken);
+            if (AuthUtils.IsBase64String(authToken))
+            {
+                var base64Bytes = System.Convert.FromBase64String(authToken);
+                var theString = System.Text.Encoding.UTF8.GetString(base64Bytes);
+                var decodedToken = JsonConvert.DeserializeObject<Base64DecodedV1Token>(theString);
+                if (String.IsNullOrEmpty(decodedToken.api_key) || String.IsNullOrEmpty(decodedToken.endpoint))
+                {
+                    throw new InvalidArgumentException("");
+                }
+                return new TokenAndEndpoints(
+                    decodedToken.api_key,
+                    "control." + decodedToken.endpoint,
+                    "cache." + decodedToken.endpoint
+                );
+            }
+            else
+            {
+                var claims = JwtUtils.DecodeJwt(authToken);
+                return new TokenAndEndpoints(
+                    authToken,
+                    claims.ControlEndpoint,
+                    claims.CacheEndpoint
+                );
+            }
         }
-        catch (InvalidArgumentException)
+        catch (Exception e) when (e is InvalidArgumentException || e is JsonReaderException)
         {
             throw new InvalidArgumentException("The supplied Momento authToken is not valid.");
         }
