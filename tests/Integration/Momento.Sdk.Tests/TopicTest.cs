117257e49cf4fc35993ab4c95bb7fe380f0680d6
Nate Anderson <nate@momentohq.com>
2023-09-01 15:56:15 -0700

    Comment out tests for troubleshooting and replace sleep with delay

--- tests/Integration/Momento.Sdk.Tests/TopicTest.cs
+++ tests/Integration/Momento.Sdk.Tests/TopicTest.cs
@@ -22,67 +22,67 @@ public class TopicTest : IClassFixture<CacheClientFixture>, IClassFixture<TopicC
         cacheName = cacheFixture.CacheName;
     }
 
-    [Theory]
-    [InlineData(null, "topic")]
-    [InlineData("cache", null)]
-    public async Task PublishAsync_NullChecksByteArray_IsError(string badCacheName, string badTopicName)
-    {
-        var response = await topicClient.PublishAsync(badCacheName, badTopicName, Array.Empty<byte>());
-        Assert.True(response is TopicPublishResponse.Error, $"Unexpected response: {response}");
-        Assert.Equal(MomentoErrorCode.INVALID_ARGUMENT_ERROR, ((TopicPublishResponse.Error)response).ErrorCode);
-    }
-
-    [Fact]
-    public async Task PublishAsync_PublishNullByteArray_IsError()
-    {
-        var response = await topicClient.PublishAsync(cacheName, "topic", (byte[])null!);
-        Assert.True(response is TopicPublishResponse.Error, $"Unexpected response: {response}");
-        Assert.Equal(MomentoErrorCode.INVALID_ARGUMENT_ERROR, ((TopicPublishResponse.Error)response).ErrorCode);
-    }
-
-    [Fact]
-    public async Task PublishAsync_BadCacheNameByteArray_IsError()
-    {
-        var response = await topicClient.PublishAsync("fake-" + cacheName, "topic", Array.Empty<byte>());
-        Assert.True(response is TopicPublishResponse.Error, $"Unexpected response: {response}");
-        Assert.Equal(MomentoErrorCode.NOT_FOUND_ERROR, ((TopicPublishResponse.Error)response).ErrorCode);
-    }
-
-    [Theory]
-    [InlineData(null, "topic")]
-    [InlineData("cache", null)]
-    public async Task PublishAsync_NullChecksString_IsError(string badCacheName, string badTopicName)
-    {
-        var response = await topicClient.PublishAsync(badCacheName, badTopicName, "value");
-        Assert.True(response is TopicPublishResponse.Error, $"Unexpected response: {response}");
-        Assert.Equal(MomentoErrorCode.INVALID_ARGUMENT_ERROR, ((TopicPublishResponse.Error)response).ErrorCode);
-    }
-
-    [Fact]
-    public async Task PublishAsync_PublishNullString_IsError()
-    {
-        var response = await topicClient.PublishAsync(cacheName, "topic", (string)null!);
-        Assert.True(response is TopicPublishResponse.Error, $"Unexpected response: {response}");
-        Assert.Equal(MomentoErrorCode.INVALID_ARGUMENT_ERROR, ((TopicPublishResponse.Error)response).ErrorCode);
-    }
-
-    [Fact]
-    public async Task PublishAsync_BadCacheNameString_IsError()
-    {
-        var response = await topicClient.PublishAsync("fake-" + cacheName, "topic", "value");
-        Assert.True(response is TopicPublishResponse.Error, $"Unexpected response: {response}");
-        Assert.Equal(MomentoErrorCode.NOT_FOUND_ERROR, ((TopicPublishResponse.Error)response).ErrorCode);
-    }
-
-    [Theory]
-    [InlineData(null, "topic")]
-    [InlineData("cache", null)]
-    public async Task SubscribeAsync_NullChecks_IsError(string badCacheName, string badTopicName)
-    {
-        var response = await topicClient.SubscribeAsync(badCacheName, badTopicName);
-        Assert.True(response is TopicSubscribeResponse.Error, $"Unexpected response: {response}");
-        Assert.Equal(MomentoErrorCode.INVALID_ARGUMENT_ERROR, ((TopicSubscribeResponse.Error)response).ErrorCode);
-    }
+    // [Theory]
+    // [InlineData(null, "topic")]
+    // [InlineData("cache", null)]
+    // public async Task PublishAsync_NullChecksByteArray_IsError(string badCacheName, string badTopicName)
+    // {
+    //     var response = await topicClient.PublishAsync(badCacheName, badTopicName, Array.Empty<byte>());
+    //     Assert.True(response is TopicPublishResponse.Error, $"Unexpected response: {response}");
+    //     Assert.Equal(MomentoErrorCode.INVALID_ARGUMENT_ERROR, ((TopicPublishResponse.Error)response).ErrorCode);
+    // }
+    //
+    // [Fact]
+    // public async Task PublishAsync_PublishNullByteArray_IsError()
+    // {
+    //     var response = await topicClient.PublishAsync(cacheName, "topic", (byte[])null!);
+    //     Assert.True(response is TopicPublishResponse.Error, $"Unexpected response: {response}");
+    //     Assert.Equal(MomentoErrorCode.INVALID_ARGUMENT_ERROR, ((TopicPublishResponse.Error)response).ErrorCode);
+    // }
+    //
+    // [Fact]
+    // public async Task PublishAsync_BadCacheNameByteArray_IsError()
+    // {
+    //     var response = await topicClient.PublishAsync("fake-" + cacheName, "topic", Array.Empty<byte>());
+    //     Assert.True(response is TopicPublishResponse.Error, $"Unexpected response: {response}");
+    //     Assert.Equal(MomentoErrorCode.NOT_FOUND_ERROR, ((TopicPublishResponse.Error)response).ErrorCode);
+    // }
+    //
+    // [Theory]
+    // [InlineData(null, "topic")]
+    // [InlineData("cache", null)]
+    // public async Task PublishAsync_NullChecksString_IsError(string badCacheName, string badTopicName)
+    // {
+    //     var response = await topicClient.PublishAsync(badCacheName, badTopicName, "value");
+    //     Assert.True(response is TopicPublishResponse.Error, $"Unexpected response: {response}");
+    //     Assert.Equal(MomentoErrorCode.INVALID_ARGUMENT_ERROR, ((TopicPublishResponse.Error)response).ErrorCode);
+    // }
+    //
+    // [Fact]
+    // public async Task PublishAsync_PublishNullString_IsError()
+    // {
+    //     var response = await topicClient.PublishAsync(cacheName, "topic", (string)null!);
+    //     Assert.True(response is TopicPublishResponse.Error, $"Unexpected response: {response}");
+    //     Assert.Equal(MomentoErrorCode.INVALID_ARGUMENT_ERROR, ((TopicPublishResponse.Error)response).ErrorCode);
+    // }
+    //
+    // [Fact]
+    // public async Task PublishAsync_BadCacheNameString_IsError()
+    // {
+    //     var response = await topicClient.PublishAsync("fake-" + cacheName, "topic", "value");
+    //     Assert.True(response is TopicPublishResponse.Error, $"Unexpected response: {response}");
+    //     Assert.Equal(MomentoErrorCode.NOT_FOUND_ERROR, ((TopicPublishResponse.Error)response).ErrorCode);
+    // }
+    //
+    // [Theory]
+    // [InlineData(null, "topic")]
+    // [InlineData("cache", null)]
+    // public async Task SubscribeAsync_NullChecks_IsError(string badCacheName, string badTopicName)
+    // {
+    //     var response = await topicClient.SubscribeAsync(badCacheName, badTopicName);
+    //     Assert.True(response is TopicSubscribeResponse.Error, $"Unexpected response: {response}");
+    //     Assert.Equal(MomentoErrorCode.INVALID_ARGUMENT_ERROR, ((TopicSubscribeResponse.Error)response).ErrorCode);
+    // }
 
     [Fact(Timeout = 5000)]
     public async Task PublishAndSubscribe_ByteArray_Succeeds()
@@ -126,100 +126,100 @@ public class TopicTest : IClassFixture<CacheClientFixture>, IClassFixture<TopicC
             return messageCount;
         }, cts.Token);
 
-        Thread.Sleep(1000);
+        await Task.Delay(1000);
 
         foreach (var value in valuesToSend)
         {
             var publishResponse = await topicClient.PublishAsync(cacheName, topicName, value);
             Assert.True(publishResponse is TopicPublishResponse.Success, $"Unexpected response: {publishResponse}");
-            Thread.Sleep(100);
+            await Task.Delay(100);
         }
 
         Assert.Equal(valuesToSend.Count, await testTask);
     }
 
-    [Fact(Timeout = 5000)]
-    public async Task PublishAndSubscribe_String_Succeeds()
-    {
-        testOutputHelper.WriteLine("starting string publish and subscribe test");
-        const string topicName = "topic_string";
-        var valuesToSend = new List<string>
-        {
-            "one",
-            "two",
-            "three",
-            "four",
-            "five"
-        };
+    // [Fact(Timeout = 5000)]
+    // public async Task PublishAndSubscribe_String_Succeeds()
+    // {
+    //     testOutputHelper.WriteLine("starting string publish and subscribe test");
+    //     const string topicName = "topic_string";
+    //     var valuesToSend = new List<string>
+    //     {
+    //         "one",
+    //         "two",
+    //         "three",
+    //         "four",
+    //         "five"
+    //     };
+    //
+    //     using var cts = new CancellationTokenSource();
+    //     cts.CancelAfter(4000);
+    //
+    //     var subscribeResponse = await topicClient.SubscribeAsync(cacheName, topicName);
+    //     Assert.True(subscribeResponse is TopicSubscribeResponse.Subscription,
+    //         $"Unexpected response: {subscribeResponse}");
+    //     var subscription = ((TopicSubscribeResponse.Subscription)subscribeResponse).WithCancellation(cts.Token);
+    //
+    //     var testTask = Task.Run(async () =>
+    //     {
+    //         var messageCount = 0;
+    //         await foreach (var message in subscription)
+    //         {
+    //             Assert.NotNull(message);
+    //             Assert.True(message is TopicMessage.Text, $"Unexpected message: {message}");
+    //
+    //             Assert.Equal(valuesToSend[messageCount], ((TopicMessage.Text)message).Value);
+    //
+    //             messageCount++;
+    //             if (messageCount == valuesToSend.Count)
+    //             {
+    //                 break;
+    //             }
+    //         }
+    //
+    //         return messageCount;
+    //     }, cts.Token);
+    //
+    //     await Task.Delay(1000);
+    //
+    //     foreach (var value in valuesToSend)
+    //     {
+    //         var publishResponse = await topicClient.PublishAsync(cacheName, topicName, value);
+    //         Assert.True(publishResponse is TopicPublishResponse.Success, $"Unexpected response: {publishResponse}");
+    //         await Task.Delay(100);
+    //     }
+    //
+    //     Assert.Equal(valuesToSend.Count, await testTask);
+    // }
 
-        using var cts = new CancellationTokenSource();
-        cts.CancelAfter(4000);
-
-        var subscribeResponse = await topicClient.SubscribeAsync(cacheName, topicName);
-        Assert.True(subscribeResponse is TopicSubscribeResponse.Subscription,
-            $"Unexpected response: {subscribeResponse}");
-        var subscription = ((TopicSubscribeResponse.Subscription)subscribeResponse).WithCancellation(cts.Token);
-
-        var testTask = Task.Run(async () =>
-        {
-            var messageCount = 0;
-            await foreach (var message in subscription)
-            {
-                Assert.NotNull(message);
-                Assert.True(message is TopicMessage.Text, $"Unexpected message: {message}");
-
-                Assert.Equal(valuesToSend[messageCount], ((TopicMessage.Text)message).Value);
-
-                messageCount++;
-                if (messageCount == valuesToSend.Count)
-                {
-                    break;
-                }
-            }
-
-            return messageCount;
-        }, cts.Token);
-
-        Thread.Sleep(1000);
-
-        foreach (var value in valuesToSend)
-        {
-            var publishResponse = await topicClient.PublishAsync(cacheName, topicName, value);
-            Assert.True(publishResponse is TopicPublishResponse.Success, $"Unexpected response: {publishResponse}");
-            Thread.Sleep(100);
-        }
-
-        Assert.Equal(valuesToSend.Count, await testTask);
-    }
-
-    [Fact(Timeout = 5000)]
-    public async Task Subscribe_EnumerateClosed_Succeeds()
-    {
-        testOutputHelper.WriteLine("starting enumerate after closed test");
-        const string topicName = "topic_closed";
-        const string messageValue = "value";
-
-        using var cts = new CancellationTokenSource();
-
-        var subscribeResponse = await topicClient.SubscribeAsync(cacheName, topicName);
-        Assert.True(subscribeResponse is TopicSubscribeResponse.Subscription,
-            $"Unexpected response: {subscribeResponse}");
-        var subscription = ((TopicSubscribeResponse.Subscription)subscribeResponse).WithCancellation(cts.Token);
-        testOutputHelper.WriteLine("subscribed");
-
-        var enumerator = subscription.GetAsyncEnumerator();
-        Assert.Null(enumerator.Current);
-        testOutputHelper.WriteLine("enumerator gotten");
-
-        var publishResponse = await topicClient.PublishAsync(cacheName, topicName, messageValue);
-        Assert.True(publishResponse is TopicPublishResponse.Success, $"Unexpected response: {publishResponse}");
-        testOutputHelper.WriteLine("message published");
-
-        cts.Cancel();
-
-        Assert.False(await enumerator.MoveNextAsync());
-
-        Assert.Null(enumerator.Current);
-    }
+    // [Fact(Timeout = 5000)]
+    // public async Task Subscribe_EnumerateClosed_Succeeds()
+    // {
+    //     testOutputHelper.WriteLine("starting enumerate after closed test");
+    //     const string topicName = "topic_closed";
+    //     const string messageValue = "value";
+    //
+    //     using var cts = new CancellationTokenSource();
+    //
+    //     var subscribeResponse = await topicClient.SubscribeAsync(cacheName, topicName);
+    //     Assert.True(subscribeResponse is TopicSubscribeResponse.Subscription,
+    //         $"Unexpected response: {subscribeResponse}");
+    //     var subscription = ((TopicSubscribeResponse.Subscription)subscribeResponse).WithCancellation(cts.Token);
+    //     testOutputHelper.WriteLine("subscribed");
+    //
+    //     var enumerator = subscription.GetAsyncEnumerator();
+    //     Assert.Null(enumerator.Current);
+    //     testOutputHelper.WriteLine("enumerator gotten");
+    //
+    //     var publishResponse = await topicClient.PublishAsync(cacheName, topicName, messageValue);
+    //     Assert.True(publishResponse is TopicPublishResponse.Success, $"Unexpected response: {publishResponse}");
+    //     testOutputHelper.WriteLine("message published");
+    //
+    //     cts.Cancel();
+    //
+    //     Assert.False(await enumerator.MoveNextAsync());
+    //
+    //     Assert.Null(enumerator.Current);
+    // }
 }
 #endif
