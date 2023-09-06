772a413ed811b1339e65d83838235d1833676e07
Pete Gautier <pete@momentohq.com>
2023-09-06 10:22:07 -0700

    chore: adding auth config

--- src/Momento.Sdk/AuthClient.cs
+++ src/Momento.Sdk/AuthClient.cs
@@ -1,11 +1,23 @@
 using System.Threading.Tasks;
+using Momento.Sdk.Auth;
 using Momento.Sdk.Auth.AccessControl;
+using Momento.Sdk.Config;
+using Momento.Sdk.Internal;
 using Momento.Sdk.Responses;
 
 namespace Momento.Sdk;
 
 public class AuthClient : IAuthClient
 {
+
+    private readonly ScsAuthClient scsAuthClient;
+
+    public AuthClient(IAuthConfiguration config, ICredentialProvider authProvider)
+    {
+        // TODO: do we need to adjust the endpoint here?
+        scsAuthClient = new ScsAuthClient(config.LoggerFactory, authProvider.AuthToken, authProvider.CacheEndpoint);
+    }
+
     public Task<GenerateDisposableTokenResponse> GenerateDisposableTokenAsync(DisposableTokenScope scope, ExpiresIn expiresIn)
     {
         throw new System.NotImplementedException();
---
+++ src/Momento.Sdk/Config/IAuthConfiguration.cs
@@ -0,0 +1,22 @@
+using Microsoft.Extensions.Logging;
+using Momento.Sdk.Config.Transport;
+
+namespace Momento.Sdk.Config;
+
+/// <summary>
+/// Contract for Auth SDK configurables.
+/// </summary>
+public interface IAuthConfiguration
+{
+    /// <inheritdoc cref="Microsoft.Extensions.Logging.ILoggerFactory" />
+    public ILoggerFactory LoggerFactory { get; }
+    /// <inheritdoc cref="Momento.Sdk.Config.Transport.ITransportStrategy" />
+    public ITransportStrategy TransportStrategy { get; }
+
+    /// <summary>
+    /// Creates a new instance of the Configuration object, updated to use the specified transport strategy.
+    /// </summary>
+    /// <param name="transportStrategy">This is responsible for configuring network tunables.</param>
+    /// <returns>Configuration object with custom transport strategy provided</returns>
+    public IAuthConfiguration WithTransportStrategy(ITransportStrategy transportStrategy);
+}
