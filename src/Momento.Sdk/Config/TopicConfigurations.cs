68b4cae16d6cd41609bcecae597706da205976f4
Pete Gautier <pete@momentohq.com>
2023-09-11 14:33:37 -0700

    chore: add topic client configurations

--- src/Momento.Sdk/Config/TopicConfigurations.cs
+++ src/Momento.Sdk/Config/TopicConfigurations.cs
@@ -70,4 +70,105 @@ public class TopicConfigurations
             return new Mobile(finalLoggerFactory, transportStrategy);
         }
     }
+
+    /// <summary>
+    /// InRegion provides defaults suitable for an environment where your client is running in the same region as the Momento
+    /// service.  It has more agressive timeouts and retry behavior than the Laptop config.
+    /// </summary>
+    public class InRegion
+    {
+        /// <summary>
+        ///  This config prioritizes throughput and client resource utilization.
+        /// </summary>
+        public class Default : TopicConfiguration
+        {
+            private Default(ILoggerFactory loggerFactory, ITopicTransportStrategy transportStrategy)
+                : base(loggerFactory, transportStrategy)
+            {
+
+            }
+
+            /// <summary>
+            /// Provides the latest recommended configuration for an InRegion environment.
+            /// </summary>
+            /// <remark>
+            /// This configuration may change in future releases to take advantage of
+            /// improvements we identify for default configurations.
+            /// </remark>
+            /// <param name="loggerFactory"></param>
+            /// <returns></returns>
+            public static ITopicConfiguration Latest(ILoggerFactory? loggerFactory = null)
+            {
+                return V1(loggerFactory);
+            }
+
+            /// <summary>
+            /// Provides the version 1 configuration for an InRegion environment.
+            /// </summary>
+            /// <remark>
+            /// This configuration is guaranteed not to change in future
+            /// releases of the Momento .NET SDK.
+            /// </remark>
+            /// <param name="loggerFactory"></param>
+            /// <returns></returns>
+            public static ITopicConfiguration V1(ILoggerFactory? loggerFactory = null)
+            {
+                var finalLoggerFactory = loggerFactory ?? NullLoggerFactory.Instance;
+                ITopicTransportStrategy transportStrategy = new StaticTopicTransportStrategy(
+                    loggerFactory: finalLoggerFactory,
+                    grpcConfig: new StaticGrpcConfiguration(deadline: TimeSpan.FromMilliseconds(1100)));
+                return new Default(finalLoggerFactory, transportStrategy);
+            }
+        }
+
+        /// <summary>
+        /// This config prioritizes keeping p99.9 latencies as low as possible, potentially sacrificing
+        /// some throughput to achieve this.  Use this configuration if the most important factor is to ensure that
+        /// unavailability doesn't force unacceptably high latencies for your own application.
+        /// </summary>
+        public class LowLatency : TopicConfiguration
+        {
+            private LowLatency(ILoggerFactory loggerFactory, ITopicTransportStrategy transportStrategy)
+                : base(loggerFactory, transportStrategy)
+            {
+
+            }
+
+            /// <summary>
+            /// Provides the latest recommended configuration for a low-latency in-region
+            /// environment.
+            /// </summary>
+            /// <remark>
+            /// This configuration may change in future releases to take advantage of
+            /// improvements we identify for default configurations.
+            /// </remark>
+            /// <param name="loggerFactory"></param>
+            /// <returns></returns>
+            public static ITopicConfiguration Latest(ILoggerFactory? loggerFactory = null)
+            {
+                return V1(loggerFactory);
+            }
+
+            /// <summary>
+            /// Provides the latest recommended configuration for a low-latency in-region
+            /// environment.
+            /// </summary>
+            /// <remark>
+            /// This configuration is guaranteed not to change in future
+            /// releases of the Momento .NET SDK.
+            /// </remark>
+            /// <param name="loggerFactory"></param>
+            /// <returns></returns>
+            public static ITopicConfiguration V1(ILoggerFactory? loggerFactory = null)
+            {
+                var finalLoggerFactory = loggerFactory ?? NullLoggerFactory.Instance;
+                ITopicTransportStrategy transportStrategy = new StaticTopicTransportStrategy(
+                    loggerFactory: finalLoggerFactory,
+                    grpcConfig: new StaticGrpcConfiguration(deadline: TimeSpan.FromMilliseconds(500))
+                );
+                return new LowLatency(finalLoggerFactory, transportStrategy);
+            }
+
+        }
+    }
 }
