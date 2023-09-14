3f3920d7120d3e4c2da24a9cb731d251074be1e8
pgautier404 <pgautier404@users.noreply.github.com>
2023-09-14 11:25:53 -0700

    chore: improve example to include predefined scope (#491)

--- examples/DisposableTokens/Program.cs
+++ examples/DisposableTokens/Program.cs
@@ -5,38 +5,85 @@ using Momento.Sdk.Auth.AccessControl;
 using Momento.Sdk.Config;
 using Momento.Sdk.Responses;
 
-ICredentialProvider authProvider = new EnvMomentoTokenProvider("MOMENTO_AUTH_TOKEN");
 
-IAuthClient client = new AuthClient(AuthConfigurations.Default.Latest(), authProvider);
-var scope = new DisposableTokenScope(Permissions: new List<DisposableTokenPermission>
+static class Program
 {
-    new DisposableToken.CacheItemPermission(
-        CacheRole.ReadWrite,
-        CacheSelector.ByName("cache"),
-        CacheItemSelector.AllCacheItems
-    ),
-    new DisposableToken.CachePermission(
-        CacheRole.ReadOnly,
-        CacheSelector.ByName("topsecret")
-    ),
-    new DisposableToken.TopicPermission(
-        TopicRole.PublishSubscribe,
-        CacheSelector.ByName("cache"),
-        TopicSelector.ByName("example-topic")
-    )
-});
-var tokenResponse = await client.GenerateDisposableTokenAsync(
-    scope,
-    ExpiresIn.Minutes(5)
-);
 
-if (tokenResponse is GenerateDisposableTokenResponse.Success token)
-{
-    Console.WriteLine("The generated disposable token is: " + token.AuthToken);
-    Console.WriteLine("The token endpoint is: " + token.Endpoint);
-    Console.WriteLine("The token expires at (epoch timestamp): " + token.ExpiresAt.Epoch());
-}
-else if (tokenResponse is GenerateDisposableTokenResponse.Error err)
-{
-    Console.WriteLine("Error generating disposable token: " + err.Message);
+    private static readonly ICredentialProvider authProvider;
+    private static readonly IAuthClient client;
+ 
+    static Program()
+    {
+        authProvider = new EnvMomentoTokenProvider("MOMENTO_AUTH_TOKEN");
+        client = new AuthClient(AuthConfigurations.Default.Latest(), authProvider);
+    }
+
+    private static async Task<GenerateDisposableTokenResponse.Success> GenerateTokenWithEnumeratedPermissions()
+    {
+        var scope = new DisposableTokenScope(Permissions: new List<DisposableTokenPermission>
+        {
+            new DisposableToken.CacheItemPermission(
+                CacheRole.ReadWrite,
+                CacheSelector.ByName("cache"),
+                CacheItemSelector.AllCacheItems
+            ),
+            new DisposableToken.CachePermission(
+                CacheRole.ReadOnly,
+                CacheSelector.ByName("topsecret")
+            ),
+            new DisposableToken.TopicPermission(
+                TopicRole.PublishSubscribe,
+                CacheSelector.ByName("cache"),
+                TopicSelector.ByName("topic")
+            )
+        });
+
+        var tokenResponse = await client.GenerateDisposableTokenAsync(
+            scope,
+            ExpiresIn.Minutes(30)
+        );
+        return ProcessResponse(tokenResponse);
+    }
+
+    private static async Task<GenerateDisposableTokenResponse.Success> GenerateTokenWithPredefinedScope()
+    {
+        var tokenResponse = await client.GenerateDisposableTokenAsync(
+            // many predefined scopes are available to easily control access to caches, cache keys, and topics
+            DisposableTokenScopes.CacheKeyReadWrite("cache", "cache-key"),
+            ExpiresIn.Minutes(30)
+        );
+        return ProcessResponse(tokenResponse);
+    }
+
+    private static GenerateDisposableTokenResponse.Success ProcessResponse(GenerateDisposableTokenResponse tokenResponse)
+    {
+        if (tokenResponse is GenerateDisposableTokenResponse.Success token)
+        {
+            return token;
+        }
+        if (tokenResponse is GenerateDisposableTokenResponse.Error err)
+        {
+            throw err.InnerException;
+        }
+        throw new Exception("Unknown response type: " + tokenResponse);
+    }
+
+    private static void PrintTokenInfo(GenerateDisposableTokenResponse.Success token)
+    {
+        Console.WriteLine(
+            "The generated disposable token (truncated): " 
+            + token.AuthToken.Substring(0, 10) 
+            + "..." 
+            + token.AuthToken.Substring((token.AuthToken.Length - 10), 10)
+        );
+        Console.WriteLine("The token expires at (epoch timestamp): " + token.ExpiresAt.Epoch() + "\n");
+    }
+
+    public async static Task Main()
+    {
+        var token = await GenerateTokenWithEnumeratedPermissions();
+        PrintTokenInfo(token);
+        token = await GenerateTokenWithPredefinedScope();
+        PrintTokenInfo(token);
+    }
 }
