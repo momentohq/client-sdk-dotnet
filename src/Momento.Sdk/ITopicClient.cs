4953482a0839d9dc44273a73e7552baea5f07873
Michael Landis <michael@momentohq.com>
2024-11-08 16:48:24 -0800

    docs: add closing param tags

--- src/Momento.Sdk/ITopicClient.cs
+++ src/Momento.Sdk/ITopicClient.cs
@@ -43,8 +43,8 @@ public interface ITopicClient : IDisposable
     /// </summary>
     /// <param name="cacheName">Name of the cache containing the topic.</param>
     /// <param name="topicName">Name of the topic.</param>
-    /// <param name="resumeAtSequenceNumber">The sequence number of the last message.
-    /// <param name="resumeAtSequencePage">The sequence page of the last message.
+    /// <param name="resumeAtSequenceNumber">The sequence number of the last message.</param>
+    /// <param name="resumeAtSequencePage">The sequence page of the last message.</param>
     /// If provided, the client will attempt to start the stream from that sequence number.</param>
     /// <returns>
     /// Task object representing the result of the subscribe operation. The
