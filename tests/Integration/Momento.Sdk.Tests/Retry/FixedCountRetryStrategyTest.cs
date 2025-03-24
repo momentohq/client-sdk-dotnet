f14d9bf37f997802995ee04f2c1f08045ed97ac1
anitarua <anita@momentohq.com>
2025-03-24 16:17:56 -0700

    refactor

--- tests/Integration/Momento.Sdk.Tests/Retry/FixedCountRetryStrategyTest.cs
+++ tests/Integration/Momento.Sdk.Tests/Retry/FixedCountRetryStrategyTest.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
 using Momento.Sdk.Auth;
 using Momento.Sdk.Config;
-using Momento.Sdk.Config.Middleware;
 using Momento.Sdk.Config.Retry;
 
 namespace Momento.Sdk.Tests.Integration.Retry;
@@ -31,67 +30,38 @@ public class FixedCountRetryStrategyTests
         _cacheConfig = Configurations.Laptop.Latest(_loggerFactory);
     }
 
-    private ICacheClient CreateClientWithMiddleware(MomentoLocalMiddleware middleware)
-    {
-        var config = _cacheConfig.WithMiddlewares(new List<IMiddleware>() { middleware });
-        var cacheClient = new CacheClient(config, _authProvider, TimeSpan.FromSeconds(60));
-        return cacheClient;
-    }
-
     [Fact]
     public void FixedCountRetryStrategy_IneligibleRpc() 
     {
-        var middleware = new MomentoLocalMiddleware(_loggerFactory, null);
-        var testMetricsCollector = middleware.TestMetricsCollector;
-        var cacheClient = CreateClientWithMiddleware(middleware);
-
-        var cacheName = Utils.NewGuidString();
-        cacheClient.CreateCacheAsync(cacheName).Wait();
-
-        cacheClient.IncrementAsync(cacheName, "key").Wait();
-        Assert.Equal(0, testMetricsCollector.GetTotalRetryCount(cacheName, MomentoRpcMethod.Increment));
+        var testProps = new MomentoLocalCacheAndCacheClient(_authProvider, _loggerFactory, _cacheConfig, null, new FixedCountRetryStrategy(_loggerFactory, 3));
+        testProps.CacheClient.IncrementAsync(testProps.CacheName, "key").Wait();
+        Assert.Equal(0, testProps.TestMetricsCollector.GetTotalRetryCount(testProps.CacheName, MomentoRpcMethod.Increment));
     }
 
     [Fact]
     public void FixedCountRetryStrategy_EligibleRpc_FullOutage() 
     {
         var maxAttempts = 3;
-        var middleware = new MomentoLocalMiddleware(_loggerFactory, new MomentoLocalMiddlewareArgs
-        {
+        var middlewareArgs = new MomentoLocalMiddlewareArgs {
             ReturnError = MomentoErrorCode.SERVER_UNAVAILABLE,
             ErrorRpcList = new List<MomentoRpcMethod> { MomentoRpcMethod.Get },
             ErrorCount = maxAttempts
-        });
-        var testMetricsCollector = middleware.TestMetricsCollector;
-        var retryStrategy = new FixedCountRetryStrategy(_loggerFactory, maxAttempts);
-        var config = _cacheConfig.WithAdditionalMiddlewares(new List<IMiddleware>() { middleware }).WithRetryStrategy(retryStrategy);
-        var cacheClient = new CacheClient(config, _authProvider, TimeSpan.FromSeconds(60));
-
-        var cacheName = Utils.NewGuidString();
-        cacheClient.CreateCacheAsync(cacheName).Wait();
-
-        cacheClient.GetAsync(cacheName, "key").Wait();
-        Assert.Equal(maxAttempts, testMetricsCollector.GetTotalRetryCount(cacheName, MomentoRpcMethod.Get));
+        };
+        var testProps = new MomentoLocalCacheAndCacheClient(_authProvider, _loggerFactory, _cacheConfig, middlewareArgs, new FixedCountRetryStrategy(_loggerFactory, maxAttempts));
+        testProps.CacheClient.GetAsync(testProps.CacheName, "key").Wait();
+        Assert.Equal(maxAttempts, testProps.TestMetricsCollector.GetTotalRetryCount(testProps.CacheName, MomentoRpcMethod.Get));
     }
 
     [Fact]
     public void FixedCountRetryStrategy_EligibleRpc_TemporaryOutage() 
     {
-        var middleware = new MomentoLocalMiddleware(_loggerFactory, new MomentoLocalMiddlewareArgs
-        {
+        var middlewareArgs = new MomentoLocalMiddlewareArgs {
             ReturnError = MomentoErrorCode.SERVER_UNAVAILABLE,
             ErrorRpcList = new List<MomentoRpcMethod> { MomentoRpcMethod.Get },
             ErrorCount = 2
-        });
-        var testMetricsCollector = middleware.TestMetricsCollector;
-        var retryStrategy = new FixedCountRetryStrategy(_loggerFactory, 3);
-        var config = _cacheConfig.WithAdditionalMiddlewares(new List<IMiddleware>() { middleware }).WithRetryStrategy(retryStrategy);
-        var cacheClient = new CacheClient(config, _authProvider, TimeSpan.FromSeconds(60));
-
-        var cacheName = Utils.NewGuidString();
-        cacheClient.CreateCacheAsync(cacheName).Wait();
-
-        cacheClient.GetAsync(cacheName, "key").Wait();
-        Assert.Equal(2, testMetricsCollector.GetTotalRetryCount(cacheName, MomentoRpcMethod.Get));
+        };
+        var testProps = new MomentoLocalCacheAndCacheClient(_authProvider, _loggerFactory, _cacheConfig, middlewareArgs, new FixedCountRetryStrategy(_loggerFactory, 3));
+        testProps.CacheClient.GetAsync(testProps.CacheName, "key").Wait();
+        Assert.Equal(2, testProps.TestMetricsCollector.GetTotalRetryCount(testProps.CacheName, MomentoRpcMethod.Get));
     }
 }
---
+++ tests/Integration/Momento.Sdk.Tests/Retry/utils/RetryTestUtils.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using Momento.Sdk.Auth;
+using Momento.Sdk.Config;
+using Momento.Sdk.Config.Middleware;
+using Momento.Sdk.Config.Retry;
+
+namespace Momento.Sdk.Tests.Integration.Retry;
+
+public class MomentoLocalCacheAndCacheClient
+{
+    public ICacheClient CacheClient { get; }
+    public string CacheName { get; }
+    public TestRetryMetricsCollector TestMetricsCollector { get; }
+
+    public MomentoLocalCacheAndCacheClient(ICredentialProvider authProvider, ILoggerFactory loggerFactory, IConfiguration cacheConfig, MomentoLocalMiddlewareArgs? args, IRetryStrategy retryStrategy)
+    {
+        var middleware = new MomentoLocalMiddleware(loggerFactory, args);
+        TestMetricsCollector = middleware.TestMetricsCollector;
+        var config = cacheConfig.WithRetryStrategy(retryStrategy).WithAdditionalMiddlewares(new List<IMiddleware>() { middleware });
+        CacheClient = new CacheClient(config, authProvider, TimeSpan.FromSeconds(60));
+        CacheName = Utils.NewGuidString();
+        CacheClient.CreateCacheAsync(CacheName).Wait();
+    }
+}
